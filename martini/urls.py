f8dcf562ac9a7e50ea721327481e7bc3466b00f2
tinker20 <bhattacharyasayan.21@gmail.com>
2015-05-29 17:08:52 +0530

    Updated the urls.py to add Google Analytics

--- martini/urls.py
+++ martini/urls.py
@@ -4,12 +4,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 import profiles.urls
 import accounts.urls
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = patterns(
     '',
     url(r'^$', views.HomePage.as_view(), name='home'),
     url(r'^about/$', views.AboutPage.as_view(), name='about'),
+    url(r'^google548d509b650f8dd7.html', TemplateView.as_view(template_name="google548d509b650f8dd7.html")),
     url(r'^', include(accounts.urls, namespace='accounts')),
     url(r'^users/', include(profiles.urls, namespace='profiles')),
     url(r'^admin/', include(admin.site.urls)),
